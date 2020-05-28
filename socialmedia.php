bf16425c2cca2484857b87990bf3b10072028683
himali gandhi <himali.gandhi@triosstudent.com>
2020-05-28 14:17:00 -0400

    first commit

---
+++ socialmedia.php
@@ -0,0 +1,485 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!--Site Metas-->
+    <meta name="description" content="A boutique non-bank financial services company specializing in providing growth solutions to small and medium-sized business" />
+    <meta name="keywords" content="Financial, CorFinancial,Business, small business, medium business " />
+    <meta name="author" content="" />
+    <meta name="viewport" content="width=device-width, intial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+       <link rel="stylesheet" href="css/bootstrap.min.css">
+       <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <script type="text/javascript" src="js/timeline.js"></script>
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <title>Social Media</title>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '691197158357706'); 
+fbq('track', 'PageView');
+</script>
+<noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id=691197158357706&ev=PageView
+&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
+    <link rel="stylesheet" href="style.css" />
+   
+    <link rel="stylesheet" href="css/footer.css" />
+    <link rel="stylesheet" href="css/main.css" />
+    <style>
+        /* .full__container {
+  margin: 20px auto;
+  display: block;
+  max-width: 980px;
+  columns: 3;
+  column-gap: 20px;
+  padding: 20px;
+}
+.full__container .post {
+  width: 100%;
+  display: block;
+  overflow: hidden;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.2);
+  margin: 0 0 20px;
+  break-inside: avoid;
+}
+.full__container .post .post__image {
+  height: 200px;
+  position: relative;
+}
+.full__container .post .post__image::before {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: black;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0) 30%);
+  z-index: 2;
+}
+.full__container .post .post__image::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: black;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.5) 100%);
+  z-index: 2;
+}
+.full__container .post .post__image img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  object-fit: cover;
+  object-position: center;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+.full__container .post .post__image .social__media_icon {
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  display: block;
+  width: 20px;
+  height: 20px;
+  overflow: hidden;
+  z-index: 3;
+}
+.full__container .post .post__image .social__media_icon i {
+  color: #fff;
+  font-size: 1.1rem;
+}
+.full__container .post .post__content {
+  padding: 15px;
+}
+.full__container .post .post__content .top__section {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin: .2rem 0 1.4rem;
+}
+.full__container .post .post__content .top__section .profile__img {
+  display: block;
+  overflow: hidden;
+  width: 40px;
+  height: 40px;
+  border-radius: 50px;
+  position: relative;
+  border: 1px solid #3498db;
+}
+.full__container .post .post__content .top__section .profile__img img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+.full__container .post .post__content .top__section h3, .full__container .post .post__content .top__section span {
+  margin: 0 0 0 .5rem;
+}
+.full__container .post .post__content .top__section h3 {
+  font-size: 1.2rem;
+  margin: 0 0 0 .5rem;
+  color: #353535;
+}
+.full__container .post .post__content .top__section span {
+  color: rgba(53, 53, 53, 0.7);
+  font-size: .9rem;
+}
+.full__container .post .post__content .bottom__section article {
+  text-transform: capitalize;
+  font-size: 1rem;
+  line-height: 1.4;
+  color: rgba(0, 0, 0, 0.9);
+}
+.full__container .post .post__content .bottom__section article .hashtag {
+  color: #8B040B;
+  font-weight: 500;
+}
+.full__container .post .post__content .bottom__section .hashtags {
+  list-style: none;
+  margin: .8rem 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+}
+.full__container .post .post__content .bottom__section .hashtags li {
+  font-size: .8rem;
+  margin: 0 .2rem;
+  color: #8B040B;
+  font-weight: bold;
+}
+.full__container .post .post__content .bottom__section .date {
+  color: rgba(0, 0, 0, 0.6);
+  font-size: .8rem;
+  padding: .2rem 0 .7rem;
+}
+.full__container .post .post__content .bottom__section .actions {
+  display: flex;
+}
+.full__container .post .post__content .bottom__section .actions .action {
+  display: flex;
+  align-items: center;
+  margin: 0 .7rem 0 0;
+}
+.full__container .post .post__content .bottom__section .actions .action i {
+  color: rgba(0, 0, 0, 0.7);
+  font-size: .8rem;
+}
+.full__container .post .post__content .bottom__section .actions .action span {
+  color: rgba(0, 0, 0, 0.5);
+  font-size: .8rem;
+  margin: 0 0 0 .3rem;
+}
+
+@media screen and (max-width: 1200px) {
+  .full__container {
+    columns: 4;
+  }
+}
+@media screen and (max-width: 992px) {
+  .full__container {
+    columns: 3;
+  }
+}
+@media screen and (max-width: 768px) {
+  .full__container {
+    columns: 2;
+  }
+}
+@media screen and (max-width: 480px) {
+  .full__container {
+    columns: 1;
+  }
+} */
+.centered{
+            position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+        }
+        </style>
+   </head>
+<body id="page-top" class="politics_version">
+
+    <?php include('header.php'); ?>
+
+
+    <div id="services" class="section lb">
+        <div class="container">
+           <img src="images/SocialMedia.jpg" alt="socialmedia" style="width:100%;"/>
+           <div class="centered">
+           <h1 style="color:white;margin-top:0;text-align:center;"><u>SOCIAL MEDIA</u>
+            </h1>
+</div>
+            <!-- <div class="section-title">
+        </div>  -->
+        
+        <!-- <div class="full__container">
+  <div class="post">
+    <div class="post__image"><img src="https://www.dropbox.com/s/tu7jo5ry8dxbeym/irene-kredenets-bFBjeuHxGjU-unsplash.jpg?raw=1"/>
+      <div class="social__media_icon"><i class="fa fa-linkedin"> </i></div>
+    </div>
+    <div class="post__content"> -->
+      <!-- <div class="top__section">
+        <div class="profile__img"><img src="https://www.dropbox.com/s/7zlj89m72yw28d1/3.jpg?raw=1"/></div>
+        <div class="profile__title">
+          <h3>Russel Inspektor</h3>
+        </div>
+      </div> -->
+      <!-- <div class="bottom__section">
+        <article>
+         You have to look at every day as a new day, keep your head up, be positive and do not give up<span class="hashtag">#CORFinancial </span></article> -->
+         <!-- <center>
+         <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6670732926075969537" height="559" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+         </center> -->
+        <!-- <ul class="hashtags">
+          <li>#CORFinancial</li>
+          <li>#financingsolutions</li>
+          <li>#businesstips</li>
+          <li>#businessfinancing</li>
+          <li>#businessloans</li>
+          <li>#businesscapital</li>
+          <li>#alternativefinancing</li>
+        </ul> -->
+        <!-- <div class="date">10:00 AM 12-02-2019</div> -->
+        <!-- <div class="actions">
+          <div class="action like"><i class="fas fa-heart"> </i><span>20</span></div>
+          <div class="action share"><i class="fas fa-share-alt"></i><span>28</span></div>
+          <div class="action comment"><i class="far fa-comment-alt"></i><span>13</span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="post">
+    <div class="post__image"><img src="https://www.dropbox.com/s/p2ncpsbrtthq24g/ilya-mirnyy-B95NRMrXN6M-unsplash.jpg?raw=1"/>
+      <div class="social__media_icon"><i class="fa fa-linkedin"> </i></div>
+    </div>
+    <div class="post__content"> -->
+      <!-- <div class="top__section">
+        <div class="profile__img"><img src="https://www.dropbox.com/s/f6o81rzo5h976h0/91.jpg?raw=1"/></div>
+        <div class="profile__title">
+          <h3>Russel Inspector</h3>
+        </div>
+      </div> -->
+      <!-- <div class="bottom__section">
+        <article>
+           You deserve a chance to start your dream, and we are here to tailor the best possible solution to fit you and your business needs. </article>
+        <ul class="hashtags">
+          <li>#ContactUs</li>
+          <li>#financingsolutions</li>
+          <li>#businesstips</li>
+          <li>#businessfinancing</li>
+          <li>#businessloans</li>
+          <li>#businesscapital</li>
+          <li>#alternatefinancing</li>
+        </ul>
+        <div class="date">10:00 AM 12-02-2019</div>
+        <div class="actions">
+          <div class="action like"><i class="fas fa-heart"> </i><span>12</span></div>
+          <div class="action share"><i class="fas fa-share-alt"></i><span>20</span></div>
+          <div class="action comment"><i class="far fa-comment-alt"></i><span>10</span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="post">
+    <div class="post__image"><img src="https://www.dropbox.com/s/5zwbkg9z99tlanj/dhiva-krishna-YApS6TjKJ9c-unsplash.jpg?raw=1"/>
+      <div class="social__media_icon"><i class="fa fa-linkedin"> </i></div>
+    </div>
+    <div class="post__content">
+      <div class="top__section"> -->
+        <!-- <div class="profile__img"><img src="https://www.dropbox.com/s/mw85mey57qbp7uk/85.jpg?raw=1"/></div>
+        <div class="profile__title">
+          <h3>Smith Doe</h3>
+        </div>
+      </div> -->
+      <!--<div class="bottom__section">
+        <article>
+           We have over 40 years of knowledge and hands-on experience , establishing and financing successful business ventures</article>
+        <ul class="hashtags">
+          <li>#AboutUs</li>
+          <li>#financingsolutions</li>
+          <li>#businestips</li>
+          <li>#businessfinancing</li>
+          <li>#businessloans</li>
+          <li>#businesscaptial</li>
+        </ul>
+        <div class="date">10:00 AM 12-02-2019</div>
+        <div class="actions">
+          <div class="action like"><i class="fas fa-heart"> </i><span>25</span></div>
+          <div class="action share"><i class="fas fa-share-alt"></i><span>30</span></div>
+          <div class="action comment"><i class="far fa-comment-alt"></i><span>10</span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="post">
+    <div class="post__image"><img src="https://www.dropbox.com/s/l480yccg0njaoln/mubariz-mehdizadeh-o-wlIFT4zE4-unsplash.jpg?raw=1"/>
+      <div class="social__media_icon"><i class="fa fa-linkedin"> </i></div>
+    </div>
+    <div class="post__content"> -->
+      <!-- <div class="top__section">
+        <div class="profile__img"><img src="https://www.dropbox.com/s/aay4xgamzytcrgy/6.jpg?raw=1"/></div>
+        <div class="profile__title">
+          <h3>Brian</h3>
+        </div>
+      </div> -->
+    |  <!-- <div class="top__section">
+        <div class="profile__img"><img src="https://www.dropbox.com/s/e93ttedjzxydrpy/18.jpg?raw=1"/></div>
+        <div class="profile__title">
+          <h3>Anya</h3>
+        </div>
+      </div> -->
+      <!-- <div class="bottom__section">
+        <article>
+           Happy Victoria Day!! <span class="hashtag">#CORFinancial</span></article>
+        <ul class="hashtags">
+          <li>#AboutUs</li>
+          <li>#financingsolutions</li>
+          <li>#businessloans</li>
+          <li>#businesscapital</li>
+          
+        </ul>
+        
+        <div class="actions">
+          <div class="action like"><i class="fas fa-heart"> </i><span>20</span></div>
+          <div class="action share"><i class="fas fa-share-alt"></i><span>20</span></div>
+          <div class="action comment"><i class="far fa-comment-alt"></i><span>10    </span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="post">
+    <div class="post__image"><img src="https://www.dropbox.com/s/ph9182a6tiecxv8/clem-onojeghuo-T6GkCfD1AtM-unsplash.jpg?raw=1"/>
+      <div class="social__media_icon"><i class="fa fa-linkedin"> </i></div>
+    </div>
+    <div class="post__content"> -->
+      <!-- <div class="top__section">
+        <div class="profile__img"><img src="https://www.dropbox.com/s/ujll5owdc11z213/41.jpg?raw=1"/></div>
+        <div class="profile__title">
+          <h3>Brick Ollen</h3>
+        </div>
+      </div> -->
+      <!-- <div class="bottom__section">
+        <article>
+           "Business Opportunities are  like buses, there's always another one coming."- Richard Branson</article>
+        <ul class="hashtags">
+          <li>#Mondaymotivation</li>
+          <li>#businessinspiration</li>
+          <li>#businessquotes</li>
+         
+        </ul>
+       
+        <div class="actions">
+          <div class="action like"><i class="fas fa-heart"> </i><span>20</span></div>
+          <div class="action share"><i class="fas fa-share-alt"></i><span>20</span></div>
+          <div class="action comment"><i class="far fa-comment-alt"></i><span>10  </span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="post">
+    <div class="post__image"><img src="https://www.dropbox.com/s/aruxxde6vc7934h/ren-ran-bBiuSdck8tU-unsplash.jpg?raw=1"/>
+      <div class="social__media_icon"><i class="fa fa-linkedin"> </i></div>
+    </div>
+    <div class="post__content"> -->
+      <!-- <div class="top__section">
+        <div class="profile__img"><img src="https://www.dropbox.com/s/1n6unthvcozcuwe/67.jpg?raw=1"/></div>
+        <div class="profile__title">
+          <h3>Russel Inspektor</h3>
+        </div>
+      </div> -->
+      <!-- <div class="bottom__section">
+        <article> We are here and ready to help you get through these challenging times</article>
+        <ul class="hashtags">
+          <li>#CORFinancial</li>
+          <li>#financingsolutions</li>
+          <li>#businesstips</li>
+          <li>#businessfinancing</li>
+          <li>#businesscapital</li>
+        </ul>
+       
+        <div class="actions">
+          <div class="action like"><i class="fas fa-heart"> </i><span>20</span></div>
+          <div class="action share"><i class="fas fa-share-alt"></i><span>20</span></div>
+          <div class="action comment"><i class="far fa-comment-alt"></i><span>10  </span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="post">
+    <div class="post__image"><img src="https://www.dropbox.com/s/9ireofotew6tw1b/mert-ozkaner-A6Otey76qKE-unsplash.jpg?raw=1"/>
+      <div class="social__media_icon"><i class="fa fa-linkedin"> </i></div>
+    </div>
+    <div class="post__content"> -->
+      <!-- <div class="top__section">
+        <div class="profile__img"><img src="https://www.dropbox.com/s/lruw4gucd4qns6m/46.jpg?raw=1"/></div>
+        <div class="profile__title">
+          <h3>George</h3>
+        </div>
+      </div> -->
+      <!-- <div class="bottom__section">
+        <article>
+           80% of Canadian startups rely on alternative funding sources</article>
+        <ul class="hashtags">
+          <li>#CORFinancial</li>
+          <li>#financingsolutions</li>
+          <li>#businesstips</li>
+          <li>#businessfinancing</li>
+          <li>#businessloans</li>
+          <li>#businesscapital</li>
+          
+        </ul>
+        
+        <div class="actions">
+          <div class="action like"><i class="fas fa-heart"> </i><span>20</span></div>
+          <div class="action share"><i class="fas fa-share-alt"></i><span>20</span></div>
+          <div class="action comment"><i class="far fa-comment-alt"></i><span>10  </span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
+ </div>
+</div>
+
+<?php include('foo.php'); ?>
+
+</body>
+</html>
+
+
+
+
+
+
